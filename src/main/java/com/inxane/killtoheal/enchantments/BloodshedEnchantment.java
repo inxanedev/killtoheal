7999e8eca58ad0ccd71beee8ae6479842ba71c07
Remigiusz Schoida <inxaneninja@gmail.com>
2023-05-08 10:19:26 +0200

    Fixed enchantment not being available in the enchanting table

--- src/main/java/com/inxane/killtoheal/enchantments/BloodshedEnchantment.java
+++ src/main/java/com/inxane/killtoheal/enchantments/BloodshedEnchantment.java
@@ -16,11 +16,6 @@ public class BloodshedEnchantment extends Enchantment {
     }
 
     @Override
-    public int getMinPower(int level) {
-        return 1;
-    }
-
-    @Override
     public int getMaxLevel() {
         return 3;
     }
